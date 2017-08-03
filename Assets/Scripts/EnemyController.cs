b36c88816bb340c945c0cdf05861de067f76ef6a
Jesse <jesse.matherne@esosolutions.com>
2017-08-03 09:28:00 -0500

    wip

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,55 +4,11 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-	public float speed = 10.0f;
 
-	// used to prevent the ship from going flush against the side of the screen
-	public float padding = 0.5f;
-
-	// used for clamping the player ship's movement
-	private float confineShipLeft;
-	private float confineShipRight;
-
-	void Start ()
+	void OnTriggerEnter2D (Collider2D col)
 	{
-		ConfineToCamera ();
+		Debug.Log ("Enemy was hit: " + col);
 	}
 
-	// Update is called once per frame
-	void Update ()
-	{
-		MoveShip ();
-	}
 
-	/// <summary>
-	/// Moves the ship using the left or right arrow keys on the keyboard.
-	/// </summary>
-	void MoveShip ()
-	{
-		// if moving left and hit left most side, move right and vice versa
-		transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
-		{
-			// could have used transform.position += Vector3.right * speed * Time.deltaTime;
-			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
-		}
-		Vector3 temp = transform.position;
-		temp.x = Mathf.Clamp (transform.position.x, confineShipLeft, confineShipRight);
-		transform.position = temp;
-
-		/* NOTES:
-			Could have used GetAxis("Horizontal") but couldn't figure out clamping the value
-		*/
-	}
-
-	/// <summary>
-	/// Confines the players movement to the main camera.
-	/// </summary>
-	void ConfineToCamera ()
-	{
-		float distance = transform.position.z - Camera.main.transform.position.z;
-		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
-		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
-		confineShipLeft = leftMost.x + padding;
-		confineShipRight = rightMost.x - padding;
-	}
 }
